734325bea07cdec506d146a3d29266bcfb3be771
Serjikdn <serjik.dn@gmail.com>
2023-11-03 20:49:56 +0200

    start

---
+++ task_01.js
@@ -0,0 +1,19 @@
+let myClick = document.querySelector('button');
+myClick.addEventListener('click', start)
+
+function start() {
+    howManyGetBurger()
+}
+
+function howManyGetBurger() {
+    let userMoneyInWallet = +prompt("Enter the amount of money in the wallet");
+    let foodItemPrice = +prompt("Enter the price of one burger");
+    let numberOfItemsToBuy = ~~(userMoneyInWallet / foodItemPrice);
+    let remainingMoney = userMoneyInWallet - (numberOfItemsToBuy * foodItemPrice);
+    alert(`You had so much money: ${userMoneyInWallet} UAH\nThe price of one burger: ${foodItemPrice} UAH\nYou can buy this many burgers: ${numberOfItemsToBuy} pcs \n The rest of the money: ${remainingMoney} UAH`)
+    taskResult.innerHTML = `You had so much money: ${userMoneyInWallet} UAH<br>
+The price of one burger: ${foodItemPrice} UAH<br>
+You can buy this many burgers: ${numberOfItemsToBuy} pcs<br>
+The rest of the money: ${remainingMoney} UAH<br>`
+}
+
---
+++ task_02.js
@@ -0,0 +1,16 @@
+let myClick = document.querySelector('button');
+myClick.addEventListener('click', start)
+
+function start() {
+ changeLastNumber ();
+}
+
+function changeLastNumber() {
+    let userNumber = +prompt("Enter your number:");
+    let lastDigit = Math.floor(userNumber % 10);
+    let number = Math.floor(userNumber / 10);
+    let result = lastDigit * Math.pow(10, Math.floor(Math.log10(number) + 1)) + number;
+    alert('Result: ' + result);
+
+    taskResult.innerHTML = `User number: ${userNumber}<br> result: ${result}`
+}
