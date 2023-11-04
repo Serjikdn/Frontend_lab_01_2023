01260a58024c4004d44bb9cbd091e38e3dbe380c
Serjikdn <serjik.dn@gmail.com>
2023-11-04 20:44:07 +0200

    start task 5

--- task_05.js
+++ task_05.js
@@ -1,1 +1,18 @@
-console.log('it is 5')
+let myClick = document.querySelector('button');
+myClick.addEventListener('click', start)
+
+function start() {
+    drawStar();
+}
+
+function drawStar() {
+    let rows = 8
+    for (let i = 1; i <= rows; i++) {
+        let star = '';
+        for (let j = 1; j <= i; j++) {
+            star += '*';
+        }
+        console.log(star);
+    }
+}
+
