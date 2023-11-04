cf2e3561be05e7d69200b958543bb062281fce2a
Serjikdn <serjik.dn@gmail.com>
2023-11-04 21:50:16 +0200

    task 6 complete

--- task_06.js
+++ task_06.js
@@ -1,1 +1,29 @@
-console.log('it is 6')
+let myClick = document.querySelector('button');
+myClick.addEventListener('click', start)
+
+function start() {
+    showSum();
+}
+
+function calcSum() {
+    let input, sum = 0;
+    let inputList = []
+
+    while (true) {
+        input = +prompt('Enter the number for sum(0- show amount):')
+        if (isNaN(input)) {
+            alert('Not is number!')
+            continue
+        }
+        if (input === 0){
+            return [sum, inputList]
+        }
+        inputList.push(input)
+        sum += input;
+    }
+}
+
+function showSum() {
+    let result = calcSum()
+    alert(`Sum of numbers: ${result[1]}\nEqual ${result[0]}`)
+}
